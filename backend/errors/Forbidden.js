235cbd008ce52d25f3d283c6b74694d74b2330cb
Hiperiosity <anastasy_ya@mail.ru>
2023-09-03 02:22:51 +0300

    на ревью

--- backend/errors/Forbidden.js
+++
@@ -1,9 +0,0 @@
-class Forbidden extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'HTTP 403 Forbidden';
-    this.statusCode = 403;
-  }
-}
-
-module.exports = Forbidden;
--- backend/errors/JsonWebTokenError.js
+++
@@ -1,9 +0,0 @@
-class JsonWebTokenError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'Access denied!';
-    this.statusCode = 401;
-  }
-}
-
-module.exports = JsonWebTokenError;
